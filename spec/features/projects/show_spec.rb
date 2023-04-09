bd1e533015eeb7c0311bc8b047641e9de36c75be
Amy Spears <amspears007@gmail.com>
2023-04-09 12:31:46 -0600

    Update tests in project show spec page to be more specific with where the content exists on page

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -8,15 +8,13 @@ RSpec.describe 'Project Show', type: :feature do
   
   describe 'User Story 1: As a visitor, when I visit ("/projects/:id")' do
     it "I see I see the project's name and material and theme of the challenge that this project belongs to." do
-      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
-      boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
       visit "/projects/#{news_chic.id}"
       save_and_open_page
 
-      expect(page).to have_content("News Chic")
-      expect(page).to have_content("Newspaper")
-      expect(page).to have_content("Recycled Material")
-      expect(page).to_not have_content("Boardfit")
+      expect(page).to have_content("Name: News Chic")
+      expect(page).to have_content("Material: Newspaper")
+      expect(page).to have_content("Challenge: Recycled Material")
+      expect(page).to_not have_content("Name: Boardfit")
     end
   end
 
